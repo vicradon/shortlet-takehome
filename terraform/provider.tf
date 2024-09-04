ccdfae6ed525c65626c36bde2493fb0882d11efa
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2024-09-04 17:38:54 +0100

    chore: add load config file for kubectl

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -37,4 +37,5 @@ provider "kubectl" {
   host                   = "https://${data.google_container_cluster.shortlet_cluster.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.shortlet_cluster.master_auth[0].cluster_ca_certificate)
+  load_config_file       = false
 }
